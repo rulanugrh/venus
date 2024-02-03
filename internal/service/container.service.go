b6196428a11cd1de61024fcca9070c31d47272d5
rulanugrh <nextarituenak01@gmail.com>
2024-02-03 20:08:22 +0700

    fix: typo

--- internal/service/container.service.go
+++ internal/service/container.service.go
@@ -10,6 +10,7 @@ import (
 
 	iservice "github.com/rulanugrh/venus/internal/service/port"
 )
+
 type containerstruct struct {
 	client *docker.Client
 }
@@ -20,17 +21,17 @@ func NewContainerService(client *docker.Client) iservice.ContainerInterface {
 	}
 }
 
-func(container *containerstruct) Create(req dto.Container, ctx context.Context) (*dao.Container, error) {
+func (container *containerstruct) Create(req dto.Container, ctx context.Context) (*dao.Container, error) {
 	data, err := container.client.CreateContainer(docker.CreateContainerOptions{
-		Name: req.Name,
+		Name:     req.Name,
 		Platform: req.Platform,
 		Config: &docker.Config{
-			Hostname: req.Config.Hostname,
-			Domainname: req.Config.Domainname,
-			Image: req.Config.Image,
-			Tty: req.Config.Tty,
-			OpenStdin: req.Config.OpenStdin,
-			Env: req.Config.Env,
+			Hostname:     req.Config.Hostname,
+			Domainname:   req.Config.Domainname,
+			Image:        req.Config.Image,
+			Tty:          req.Config.Tty,
+			OpenStdin:    req.Config.OpenStdin,
+			Env:          req.Config.Env,
 			ExposedPorts: req.Config.Port,
 		},
 		Context: ctx,
@@ -39,54 +40,54 @@ func(container *containerstruct) Create(req dto.Container, ctx context.Context) 
 	if err != nil {
 		return nil, web.Error{
 			Message: "Tidak bisa membuat container ke docker daemon",
-			Code: 400,
+			Code:    400,
 		}
 	}
 
 	errs := container.client.StartContainer(data.ID, &docker.HostConfig{
-		Binds: req.HostConfig.Binds,
+		Binds:        req.HostConfig.Binds,
 		PortBindings: req.HostConfig.PortBinding,
-		NetworkMode: req.HostConfig.NetworkMode,
+		NetworkMode:  req.HostConfig.NetworkMode,
 	})
 
 	if errs != nil {
 		return nil, web.Error{
 			Message: errs.Error(),
-			Code: 400,
+			Code:    400,
 		}
 	}
 
 	response := dao.Container{
-		ID: data.ID,
-		Image: data.Image,
-		Created: data.Created,
-		Path: data.Path,
+		ID:           data.ID,
+		Image:        data.Image,
+		Created:      data.Created,
+		Path:         data.Path,
 		HostnamePath: data.HostnamePath,
-		HostsPath: data.HostsPath,
+		HostsPath:    data.HostsPath,
 		Config: &dao.Config{
-			Hostname: data.Config.Hostname,
+			Hostname:   data.Config.Hostname,
 			Domainname: data.Config.Domainname,
-			Tty: data.Config.Tty,
-			OpenStdin: data.Config.OpenStdin,
-			Env: data.Config.Env,
-			Image: data.Image,
-			Port: data.Config.ExposedPorts,
+			Tty:        data.Config.Tty,
+			OpenStdin:  data.Config.OpenStdin,
+			Env:        data.Config.Env,
+			Image:      data.Image,
+			Port:       data.Config.ExposedPorts,
 		},
 	}
 
 	return &response, nil
 }
 
-func(container *containerstruct) ListContainer() ([]dao.ListContainer, error) {
+func (container *containerstruct) ListContainer() ([]dao.ListContainer, error) {
 	data, err := container.client.ListContainers(docker.ListContainersOptions{
-		All: true,
-		Limit: 10,	
+		All:   true,
+		Limit: 10,
 	})
 
 	if err != nil {
 		return nil, web.Error{
 			Message: "Tidak bisa melihat semua service di docker daemon",
-			Code: 400,
+			Code:    400,
 		}
 	}
 
@@ -96,9 +97,9 @@ func(container *containerstruct) ListContainer() ([]dao.ListContainer, error) {
 		for _, ports := range result.Ports {
 			port := dao.Port{
 				PrivatePort: ports.PrivatePort,
-				PublicPort: ports.PublicPort,
-				IP: ports.IP,
-				Type: ports.Type,
+				PublicPort:  ports.PublicPort,
+				IP:          ports.IP,
+				Type:        ports.Type,
 			}
 
 			listPorts = append(listPorts, port)
@@ -106,13 +107,13 @@ func(container *containerstruct) ListContainer() ([]dao.ListContainer, error) {
 		}
 
 		singleContainer := dao.ListContainer{
-			ID: result.ID,
-			Image: result.Image,
-			Status: result.Status,
+			ID:      result.ID,
+			Image:   result.Image,
+			Status:  result.Status,
 			Command: result.Command,
-			State: result.State,
+			State:   result.State,
 			Created: result.Created,
-			Ports: listPorts,
+			Ports:   listPorts,
 		}
 
 		response = append(response, singleContainer)
@@ -121,43 +122,42 @@ func(container *containerstruct) ListContainer() ([]dao.ListContainer, error) {
 	return response, nil
 }
 
-func(container *containerstruct) InspectContainer(id string) (*dao.InspectContainer, error) {
+func (container *containerstruct) InspectContainer(id string) (*dao.InspectContainer, error) {
 	data, err := container.client.InspectContainer(id)
 	if err != nil {
 		return nil, web.Error{
 			Message: err.Error(),
-			Code: 400,
+			Code:    400,
 		}
 	}
 
 	response := dao.InspectContainer{
-		ID: data.ID,
-		Image: data.Image,
+		ID:           data.ID,
+		Image:        data.Image,
 		HostnamePath: data.HostnamePath,
-		HostsPath: data.HostsPath,
-		Name: data.Name,
-		Tty: data.Config.Tty,
-		OpenStdin: data.Config.OpenStdin,
-		Env: data.Config.Env,
-		Port: data.Config.ExposedPorts,
+		HostsPath:    data.HostsPath,
+		Name:         data.Name,
+		Tty:          data.Config.Tty,
+		OpenStdin:    data.Config.OpenStdin,
+		Env:          data.Config.Env,
+		Port:         data.Config.ExposedPorts,
 	}
 
 	return &response, nil
 }
 
-func(container *containerstruct) DeleteContaienr(id string, ctx context.Context) error {
+func (container *containerstruct) DeleteContaienr(id string, ctx context.Context) error {
 	err := container.client.RemoveContainer(docker.RemoveContainerOptions{
 		Context: ctx,
-		ID: id,
+		ID:      id,
 	})
 
 	if err != nil {
 		return web.Error{
 			Message: err.Error(),
-			Code: 400,
+			Code:    400,
 		}
 	}
 
 	return nil
 }
-
